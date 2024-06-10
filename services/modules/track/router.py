b4c68af2a726a5818813f5e3a714e919b4437c0f
Leonardho R Sitanggang <79736302+FlazeFy@users.noreply.github.com>
2024-06-11 06:10:58 +0700

    [BE] Api get last track, get total distance travelled by month

--- services/modules/track/router.py
+++ services/modules/track/router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, HTTPException
-from services.modules.track.track_queries import get_tracks, get_tracks_period
+from services.modules.track.track_queries import get_tracks, get_tracks_period, get_last_tracker_position_api, get_track_by_year_query
 from datetime import datetime
 from pydantic import BaseModel
 
@@ -7,9 +7,23 @@ router_track = APIRouter()
 
 # GET Query
 @router_track.get("/api/v1/track/journey/{id}", response_model=dict)
-async def get_all_my_project_v1(id: str):
+async def get_current_track(id: str):
     try:
         return await get_tracks(userId=id)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@router_track.get("/api/v1/track/last/{id}", response_model=dict)
+async def get_last_track(id: str):
+    try:
+        return await get_last_tracker_position_api(userId=id)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@router_track.get("/api/v1/track/year/{year}/{id}", response_model=dict)
+async def get_track_by_year(year:int, id: str):
+    try:
+        return await get_track_by_year_query(userId=id, year=year)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     
@@ -17,7 +31,7 @@ class TrackPeriodRequest(BaseModel):
     start_time: datetime
     end_time: datetime
 @router_track.get("/api/v1/track/journey/period/{id}", response_model=dict)
-async def get_all_my_project_v1(id: str, request: TrackPeriodRequest):
+async def get_track_journey(id: str, request: TrackPeriodRequest):
     try:
         return await get_tracks_period(userId=id, start_time=request.start_time, end_time=request.end_time)
     except Exception as e:
--- services/modules/track/track_queries.py
+++ services/modules/track/track_queries.py
@@ -1,11 +1,14 @@
 import firebase_admin
-from firebase_admin import credentials
 from firebase_admin import db
 from datetime import datetime, timedelta
+from helpers.converter import calculate_distance
 
 async def get_last_tracker_position():
+    # Attribute
     userId = 'fcd3f23e-e5aa-11ee-892a-3216422910e9'
     ref = db.reference('track_live_'+userId)
+
+    # Order child index
     query = ref.order_by_child('created_at').limit_to_last(1)
     data = query.get()
 
@@ -20,8 +23,31 @@ async def get_last_tracker_position():
             return track_lat, track_long, msg
     return None
 
-async def get_tracks(userId:str):
+async def get_last_tracker_position_api(userId:str):
+    # Attribute
+    userId = 'fcd3f23e-e5aa-11ee-892a-3216422910e9'
     ref = db.reference('track_live_'+userId)
+
+    # Order child index
+    query = ref.order_by_child('created_at').limit_to_last(1)
+    data = query.get()
+
+    if data:
+        data_values = list(data.values())
+        return {
+            "data": data_values[0],
+            "message": "Last track found",
+        }
+    else:
+        return {
+            "message": "No Track found",
+        }
+
+async def get_tracks(userId:str):
+    # Attribute
+    ref = db.reference('track_live_'+userId)
+
+    # Order child index
     query = ref.order_by_child('created_at').limit_to_last(10)
     data = query.get()
 
@@ -38,10 +64,13 @@ async def get_tracks(userId:str):
         }
     
 async def get_tracks_period(userId: str, start_time: datetime = None, end_time: datetime = None):
+    # Attribute
     ref = db.reference('track_live_' + userId)
     
+    # Order child index
     query = ref.order_by_child('created_at')
     
+    # Filter
     if start_time:
         start_time_str = start_time.isoformat(timespec='microseconds')
         query = query.start_at(start_time_str)
@@ -63,3 +92,88 @@ async def get_tracks_period(userId: str, start_time: datetime = None, end_time: 
         return {
             "message": "No Track journey found",
         }
+    
+async def get_track_by_year_query(userId: str, year: int):
+    # Attribute
+    ref = db.reference('track_live_' + userId)
+    start_of_year = datetime(year, 1, 1)
+    end_of_year = datetime(year, 12, 31, 23, 59, 59, 999999)
+    
+    # Filter
+    start_time_str = start_of_year.isoformat(timespec='microseconds')
+    end_time_str = end_of_year.isoformat(timespec='microseconds')
+    
+    # Order child index
+    query = ref.order_by_child('created_at').start_at(start_time_str).end_at(end_time_str)
+    
+    data = query.get()
+    
+    if data:
+        data_values = list(data.values())
+        
+        # Column builder
+        for item in data_values:
+            created_at = datetime.fromisoformat(item['created_at'])
+            item['month'] = created_at.month
+            if 'battery_indicator' in item:
+                del item['battery_indicator']
+            if 'created_by' in item:
+                del item['created_by']
+            if 'track_type' in item:
+                del item['track_type']
+        
+        # Order by column builder
+        data_values.sort(key=lambda x: x['month'])
+        
+        # Holder
+        month_distances = {}
+        
+        for i in range(len(data_values) - 1):
+            data_values[i]['coordinate_start'] = f"{data_values[i]['track_lat']},{data_values[i]['track_long']}"
+            data_values[i]['coordinate_end'] = f"{data_values[i + 1]['track_lat']},{data_values[i + 1]['track_long']}"
+            data_values[i]['distance'] = calculate_distance(data_values[i]['coordinate_start'], data_values[i]['coordinate_end'])
+            
+            month = data_values[i]['month']
+            if month in month_distances:
+                month_distances[month] += data_values[i]['distance']
+            else:
+                month_distances[month] = data_values[i]['distance']
+        
+        if data_values:
+            last_item = data_values[-1]
+            last_item['coordinate_start'] = f"{last_item['track_lat']},{last_item['track_long']}"
+            last_item['coordinate_end'] = None 
+            last_item['distance'] = 0 
+            month = last_item['month']
+            if month in month_distances:
+                month_distances[month] += last_item['distance']
+            else:
+                month_distances[month] = last_item['distance']
+        
+        # Final res
+        formatted_data = [{"context": month, "total": distance} for month, distance in month_distances.items()]
+        
+        # Column builder
+        for item in data_values:
+            if 'track_lat' in item:
+                del item['track_lat']
+            if 'track_long' in item:
+                del item['track_long']
+            if 'coordinate_start' in item:
+                del item['coordinate_start']
+            if 'coordinate_end' in item:
+                del item['coordinate_end']
+            if 'distance' in item:
+                del item['distance']
+        
+        return {
+            "data": formatted_data,
+            "data_detail": data_values,
+            "message": "Track journey found",
+            "count": len(data_values)
+        }
+    else:
+        return {
+            "message": "No Track journey found",
+        }
+
